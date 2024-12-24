6395f8b51890c5ffb06153fe7d0d4be8bf5fba2a
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-12-24 20:30:55 +0700

    add translation

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module IWedding
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
   end
 end
