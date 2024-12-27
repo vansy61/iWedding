3a7ca83f4578614010c52e9e8960439c3f97ee7b
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-12-27 13:13:22 +0700

    tailwindcss animation heart zoom

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,10 +11,38 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        dancing: ['Dancing Script', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        themeColor: '#8673ff'
-      }
+        themeColor: '#8673ff',
+      },
+      keyframes: {
+        'heart-zoom': {
+          '0%': {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'scale(2)',
+            opacity: '0',
+          },
+        },
+        'heart-zoom-1': {
+          '0%': {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'scale(3)',
+            opacity: '0',
+          },
+        },
+      },
+      animation: {
+        'heart-zoom-1': 'heart-zoom-1 2s ease-in-out infinite',
+        'heart-zoom': 'heart-zoom 2s ease-in-out infinite',
+      },
+
     },
   },
   plugins: [
