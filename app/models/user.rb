7ef8d89233714447678f8f24f246cd87abb6238b
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-12-28 17:15:13 +0700

    auto create wedding info after create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :wedding_info, dependent: :destroy
+  after_create :create_wedding_info
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create wedding_info after user creation" do
+    user = User.create(
+      email: "test@example.com",
+      password: "password123",
+      password_confirmation: "password123"
+    )
+
+    assert user.wedding_info.present?, "Wedding info was not created"
+  end
 end
